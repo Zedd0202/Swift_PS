aa07b44067f34c6cbcf1ee89fdcdcc7aedce6cfb
Zedd0202 <ch41150@hanmail.net>
2017-05-20 15:24:36 +0900

    1000,1001,11718

---
+++ SwiftPratice/main.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+
+while true{
+    
+    var read =  readLine()
+    if var sol = read{
+        print(read!)
+    }
+    else {
+        break
+
+    }
+}
+
+/*while let line = readLine() {
+    print(line)
+}
+*/
+ 
