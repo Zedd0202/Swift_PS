a500838ecf66ece495e35abbb238989265a99ed9
Zedd0202 <ch41150@hanmail.net>
2017-09-12 15:12:01 +0900

    10821 정수의 개수

---
+++ SwiftPratice/정수의개수.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 9. 12..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+print(readLine()!.components(separatedBy: ",").count)
+
