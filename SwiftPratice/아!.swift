3ed330f7e72bead246a7dcc1722d880e0b9eb9d8
Zedd0202 <ch41150@hanmail.net>
2017-09-12 18:29:55 +0900

    4999 아

---
+++ SwiftPratice/아!.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 9. 12..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+var ja = readLine()!
+var doctor = readLine()!
+if ja.characters.count < doctor.characters.count{
+    print("no")
+}
+else{
+    print("go")
+}
