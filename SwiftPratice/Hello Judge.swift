15afc4e5cbe2d961172854250e6242cbea6d9c8c
Zedd0202 <ch41150@hanmail.net>
2017-09-12 14:36:57 +0900

    9316 Hello Judge

---
+++ SwiftPratice/Hello Judge.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 9. 12..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+var a = Int(readLine()!)!
+for i in 1...a{
+    print("Hello World, Judge" + " \(i)"+"!")
+}
