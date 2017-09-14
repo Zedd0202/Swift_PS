6c1f37bc557c390db457aa1a46407f2a00f92961
Zedd0202 <ch41150@hanmail.net>
2017-09-14 18:20:03 +0900

    2743 단어길이재기

---
+++ SwiftPratice/단어길이재기.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 9. 14..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+print(readLine()!.characters.count)
