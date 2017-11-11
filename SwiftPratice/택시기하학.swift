54c7b240b6fd12edd1e77721b65920ddc5db5633
Zedd0202 <ch41150@hanmail.net>
2017-11-11 15:44:03 +0900

    3053 택시 기하학

---
+++ SwiftPratice/택시기하학.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 11. 11..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+var a = Double(readLine()!)!
+print(String(format: "%.6f", a*a*M_PI))
+
+print(String(format: "%.6f", a*a*2.0))
