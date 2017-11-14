7f0a54eda1068f6858e714e14587bf93979affb8
Zedd0202 <ch41150@hanmail.net>
2017-11-14 19:33:22 +0900

    1550 16진수

---
+++ SwiftPratice/16진수.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 11. 14..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+var a = readLine()!
+var b = Int(a,radix: 16)!
+print(b)
