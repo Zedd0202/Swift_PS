c5773402ac12860f649e2e349b9e9ba058cc04d8
Zedd0202 <ch41150@hanmail.net>
2017-11-17 21:43:09 +0900

    11179 2진수 뒤집기

---
+++ SwiftPratice/2진수 뒤집기.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 11. 17..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+print(Int(String((String(Int(readLine()!)!,radix : 2)).characters.reversed()),radix:2)!)
+
+
