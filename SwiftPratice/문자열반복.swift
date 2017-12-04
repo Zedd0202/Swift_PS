aa36ef378087bb77893ad6fcddc157b1f12cbcef
Zedd0202 <ch41150@hanmail.net>
2017-12-04 23:21:35 +0900

    문자열반복 like Swift

--- SwiftPratice/문자열반복.swift
+++ SwiftPratice/문자열반복.swift
@@ -8,17 +8,27 @@
 
 import Foundation
 let test = Int(readLine()!)!
-for _ in 1...test{
-    var str = ""
-
-    let input = readLine()!.components(separatedBy: " ")
-    let a = Int(input[0])!
-    let arr = Array(input[1].characters)
-    for index in arr{
-        for _ in 1...a{
-            str += String(index)
-        }
-    }
-    print(str)
+typealias completion = (String)->()
+func output(value:String){
+    print(value)
 }
+func sol(completion: completion){
+        var str = ""
+        let input = readLine()!.components(separatedBy: " ")
+        let a = Int(input[0])!
+        let arr = Array(input[1].characters)
+        for index in arr{
+            for _ in 1...a{
+                str += String(index)
+            }
+        }
+        completion(str)
+    
+}
+for _ in 1...test{
+    sol(completion: { (str) in
+        output(value: str)
+    })
+}
+
 
