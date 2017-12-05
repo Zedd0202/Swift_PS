1bdfe7f1c5f640362ecde8a145318ca9b7bbf4d5
Zedd0202 <ch41150@hanmail.net>
2017-12-05 13:53:38 +0900

    5218 알파벳거리

---
+++ SwiftPratice/알파벳거리.swift
@@ -0,0 +1,56 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 12. 5..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+extension Character
+{
+    func unicodeScalarCodePoint() -> UInt32
+    {
+        let characterString = String(self)
+        let scalars = characterString.unicodeScalars
+        
+        return scalars[scalars.startIndex].value
+    }
+}
+typealias processCompletion = ([Int])->()
+typealias inputCompletion = ([Character],[Character])->()
+let test = Int(readLine()!)!
+func input(completion : inputCompletion){
+    var input = readLine()!.components(separatedBy: " ")
+    let a = Array(input[0].characters)
+    let b = Array(input[1].characters)
+    completion(a,b)
+}
+func process(completion : processCompletion){
+    input { (a, b) in
+        var sol = [Int]()
+        for index in 0..<a.count{
+            let aUni = Int(a[index].unicodeScalarCodePoint()-65)
+            let bUni = Int(b[index].unicodeScalarCodePoint()-65)
+            if aUni <= bUni {
+                sol.append(bUni-aUni)
+            }
+            else{
+                sol.append(bUni+26-aUni)
+            }
+        }
+        completion(sol)
+    }
+    
+
+}
+for _ in 1...test{
+    process(completion: { (sol) in
+        print("Distances:", terminator: " ")
+        for index in sol{
+            print(index,terminator:" ")
+        }
+        print()
+    })
+}
+
