c1e6423227bfa59ac6d9d3a6d39ecea945c30245
Zedd0202 <ch41150@hanmail.net>
2017-12-12 23:52:47 +0900

    11654 아스키코드

---
+++ SwiftPratice/아스키코드.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 12. 12..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+print(UnicodeScalar(readLine()!)!.value)
