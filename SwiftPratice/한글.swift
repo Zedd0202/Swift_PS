afbc345d932d497b0e46b4879795c0f2ef0f2823
Zedd0202 <ch41150@hanmail.net>
2017-12-14 20:57:01 +0900

    11282 한글

---
+++ SwiftPratice/한글.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 12. 13..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+var num = Int(readLine()!)!+44031
+if let num = UnicodeScalar(num){
+    print(num)
+}
