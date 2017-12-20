844896164c46037db5fd63fc90a832d5604c1b3d
Zedd0202 <ch41150@hanmail.net>
2017-12-20 14:01:50 +0900

    11729 하노이탑 이동순서

---
+++ SwiftPratice/하노이탑.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 12. 20..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+var num = Int(readLine()!)!
+print(Int(pow(2.0, Double(num))-1))
+func hanoi(n : Int, a : Int, b:Int, c: Int){
+    if n == 1{
+        print(a,c)
+        return
+    }
+    hanoi(n: n-1, a: a, b: c, c: b)
+    print(a,c)
+    hanoi(n: n-1, a: b, b: a, c: c)
+}
+hanoi(n: num, a: 1, b: 2, c: 3)
