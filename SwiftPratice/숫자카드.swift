9e117ce151e92553f3ad926aa2de69266a392270
Zedd0202 <ch41150@hanmail.net>
2017-12-22 15:48:09 +0900

    10815 숫자카드 function composition ver.

---
+++ SwiftPratice/숫자카드.swift
@@ -0,0 +1,48 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 12. 20..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+typealias completionHandler = ([Int],[Int])->()
+func process(completion: completionHandler){
+    let test = Int(readLine()!)!
+    let sangArr = readLine()!.components(separatedBy: " ").map{Int($0)!}.sorted()
+    let num = Int(readLine()!)!
+    let testArr = readLine()!.components(separatedBy: " ").map{Int($0)!}
+    completion(sangArr, testArr)
+}
+func output(value:Bool){
+    value == true ? print(1,terminator: " ") : print(0,terminator: " ")
+}
+func binarySearch(_ sangArr :[Int], _ testArr : [Int]){
+    zedd :   for index in testArr{
+        
+        var low = 0
+        var high = sangArr.count-1
+        while(low<=high){
+            let mid = (low+high)/2
+            if sangArr[mid] == index {
+                output(value: true)
+                continue zedd
+            }
+            else{
+                if sangArr[mid] > index{
+                    high = mid - 1
+                }
+                else{
+                    low = mid + 1
+                }
+            }
+        }
+        output(value: false)
+
+    }
+}
+process { (sangArr, testArr) in
+    binarySearch(sangArr,testArr)
+}
+
--- SwiftPratice/하노이탑.swift
+++ SwiftPratice/하노이탑.swift
@@ -7,15 +7,16 @@
 //
 
 import Foundation
-var num = Int(readLine()!)!
-print(Int(pow(2.0, Double(num))-1))
-func hanoi(n : Int, a : Int, b:Int, c: Int){
-    if n == 1{
-        print(a,c)
-        return
-    }
-    hanoi(n: n-1, a: a, b: c, c: b)
-    print(a,c)
-    hanoi(n: n-1, a: b, b: a, c: c)
-}
-hanoi(n: num, a: 1, b: 2, c: 3)
+//var num = Int(readLine()!)!
+//print(Int(pow(2.0, Double(num))-1))
+//func hanoi(n : Int, a : Int, b:Int, c: Int){
+//    if n == 1{
+//        print(a,c)
+//        return
+//    }
+//    hanoi(n: n-1, a: a, b: c, c: b)
+//    print(a,c)
+//    hanoi(n: n-1, a: b, b: a, c: c)
+//}
+//hanoi(n: num, a: 1, b: 2, c: 3)
+
