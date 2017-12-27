a625d67da010ace465a1d81bc285d0f8bdac92ad
Zedd0202 <ch41150@hanmail.net>
2017-12-27 13:14:14 +0900

    8393 합

---
+++ SwiftPratice/합8393.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2017. 12. 26..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+import Foundation
+var num = Int(readLine()!)!
+print((num*(num+1))/2)
