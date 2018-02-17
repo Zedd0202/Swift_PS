0997b5a01e09356eaada772c957fb302b9f87716
Zedd0202 <ch41150@hanmail.net>
2018-02-17 18:28:27 +0900

    14491 9진수

---
+++ SwiftPratice/9진수.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2018. 2. 17..
+//  Copyright © 2018년 Zedd. All rights reserved.
+//
+
+import Foundation
+var input = Int(readLine()!)!
+print(String(input, radix : 9))
