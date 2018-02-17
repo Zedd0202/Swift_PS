f809bc81c8014f9d58a85c373ac2b68cf9b81628
Zedd0202 <ch41150@hanmail.net>
2018-02-17 18:06:47 +0900

    14581 팬들에게 둘러싸인 홍준

---
+++ SwiftPratice/팬들에게 둘러싸인 홍준.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2018. 1. 30..
+//  Copyright © 2018년 Zedd. All rights reserved.
+//
+
+import Foundation
+var input = readLine()!
+print("""
+:fan::fan::fan:
+:fan::\(input)::fan:
+:fan::fan::fan:
+""")
