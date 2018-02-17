d5399012d64693fdfc6556f9eca6f686678b0dc1
Zedd0202 <ch41150@hanmail.net>
2018-02-17 23:48:53 +0900

    13297 Quick Estimates

---
+++ SwiftPratice/Quick Estimates .swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2018. 2. 17..
+//  Copyright © 2018년 Zedd. All rights reserved.
+//
+
+import Foundation
+for _ in 0..<Int(readLine()!)!{
+    print(Array(readLine()!).count)
+}
