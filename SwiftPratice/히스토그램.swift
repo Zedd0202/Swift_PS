a634a05ed60b66638c0820e523c0cb49004dbdb3
Zedd0202 <ch41150@hanmail.net>
2018-02-17 23:55:08 +0900

    13752 히스토그램

---
+++ SwiftPratice/히스토그램.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2018. 2. 17..
+//  Copyright © 2018년 Zedd. All rights reserved.
+//
+
+import Foundation
+for _ in 0..<Int(readLine()!)!{
+    let num = Int(readLine()!)!
+    for _ in 0..<num{
+        print("=", terminator:"")
+    }
+    print()
+}
