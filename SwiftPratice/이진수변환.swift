ab89c6600ef7e097cc381d26f8323ed2491e3c31
Zedd <ch41150@hanmail.net>
2018-12-11 13:03:11 +0900

    10829 이진수변환

---
+++ SwiftPratice/이진수변환.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 11/12/2018.
+//  Copyright © 2018 Zedd. All rights reserved.
+//
+
+import Foundation
+let input = u_long(readLine()!)!
+print(String(input, radix: 2))
+
