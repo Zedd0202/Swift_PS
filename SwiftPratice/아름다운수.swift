6e296741b0a9f02e6ea589adc71f2ca8c210d22d
Zedd <ch41150@hanmail.net>
2019-02-24 15:48:41 +0900

    2774 아름다운 수

---
+++ SwiftPratice/아름다운수.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 24/02/2019.
+//  Copyright © 2019 Zedd. All rights reserved.
+//
+
+import Foundation
+let test = Int(readLine()!)!
+for _ in 0..<test {
+    let num = Set(Array(readLine()!))
+    print(num.count)
+}
+
