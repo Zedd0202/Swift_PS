1dd479ceb67c19319659f7b7806ff95dc78e162b
Zedd0202 <ch41150@hanmail.net>
2020-03-19 14:06:20 +0900

    18108 1998년생인 내가 태국에서는 2541년생?

---
+++ SwiftPratice/1998년생인 내가 태국에서는 2541년생?!.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  SwiftPratice
+//
+//  Created by Zedd on 2020/03/19.
+//  Copyright © 2020 Zedd. All rights reserved.
+//
+
+import Foundation
+print(Int(readLine()!)! - 543)
